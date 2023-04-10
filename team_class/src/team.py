c91718e9f0409807bcae2ab3f04d59b403a83b45
Angel <angelgonzc@yahoo.com>
2023-04-10 16:08:31 +0100

    homework done

---
+++ team_class/src/team.py
@@ -0,0 +1,22 @@
+
+class Team:
+    def __init__(self, input_name, input_players, input_coach):
+        self.name = input_name
+        self.players = input_players
+        self.coach = input_coach
+        self.points = 0
+
+    def add_player(self, new_player):
+        self.players.append(new_player)
+
+    def has_player(self, player):
+        if player in self.players:
+            return True
+        else:
+            return False
+    
+    def play_game(self, has_won):
+        if has_won == True:
+            self.points += 3
+        elif has_won == False:
+            self.points += 0
